b395a614da6ccc03b33514f25a13711d6a8c1ac6
doctorlilo <lironkatz4@gmail.com>
2025-12-30 15:47:48 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -3,4 +3,14 @@ def split_before_each_uppercases(formula):
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location =len(formula) 
+    for i, char in enumerate(formula):
+      if char.isdigit():
+        digit_location = i
+        prefix = formula[0: digit_location]
+        numeric = formula[digit_location: len(formula)]
+        break
+      else:
+        prefix = formula[0: digit_location]
+        numeric = 1
+    print(prefix, numeric)
